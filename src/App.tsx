568ec5ccc68ef5d8485532b937bc03c3235ef332
Nicolas GIRARD <NG38@outlook.fr>
2025-04-10 12:15:40 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import './App.css'
+import EmployeeCard from "./EmployeeCard.tsx";
+import {useState} from "react";
+
+const sampleEmployee = {
+    name: {
+        first: "Nicolas",
+        last: "GIRARD",
+    },
+    email: "nicolas.girard@example.com",
+    picture: {
+        medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
+    },
+};
+
+function App() {
+    const [employees, setEmployees] = useState(sampleEmployee);
+
+    const getEmployee = () => {
+        // Send the request
+        fetch("https://randomuser.me/api?nat=en")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);setEmployees(data.results[0]);
+            });
+    };
+
+  return (
+      <div className='App'>
+          <EmployeeCard employee={employees} />
+          <button type="button" onClick={getEmployee}>Get employee</button>
+      </div>
+  )
+}
+
+export default App
